05f412b7f5acfcdca0f603059b777bc96b862332
billiejoenahema <billiejoenahema@gmail.com>
2021-07-25 14:55:39 +0900

    create: Task table

--- database/migrations/2021_07_25_141249_create_tasks_table.php
+++ database/migrations/2021_07_25_141249_create_tasks_table.php
@@ -15,6 +15,8 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->boolean('is_done')->default(false);
             $table->timestamps();
         });
     }
