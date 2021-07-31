a847ee7686596db6a3d14dadb66bb18a77a9b9b8
billiejoenahema <billiejoenahema@gmail.com>
2021-07-31 18:33:41 +0900

    create: index.tsx

---
+++ resources/ts/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Todo SPA</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
