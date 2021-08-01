33f29080a77650f4a48321ca4e991cf9bb1ea7aa
billiejoenahema <billiejoenahema@gmail.com>
2021-08-01 20:06:07 +0900

    fix: link&src add quotation

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Todo SPA</title>
-        <link rel="stylesheet" href="{{mix(css/app.css)}}">
+        <link rel="stylesheet" href="{{mix('css/app.css')}}">
     </head>
     <body>
       <div id="app"></div>
     </body>
-    <script src="{{mix(js/index.js)}}"></script>
+    <script src="{{mix('js/index.js')}}"></script>
 </html>
