e37b0452de3cef2b9c6499186851154d5478e5a0
billiejoenahema <billiejoenahema@gmail.com>
2021-08-02 20:05:20 +0900

    create: router component

--- resources/ts/App.tsx
+++ resources/ts/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import Router from './router'
 
 const App = () => {
   return (
-    <div>
-      <h1>Todo SPA</h1>
-    </div>
+    <Router />
   )
 }
 export default App
